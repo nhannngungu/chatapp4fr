45accc12c19bfc88456b70f91e4daf337f8e37eb
DoThanhNhan2280602162 <dothanhnhan9370@gmail.com>
2026-02-10 01:44:47 +0700

    Fix double slash url

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://chatapp4fr.onrender.com/";
+export const host = "https://chatapp4fr.onrender.com";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setavatar`;
